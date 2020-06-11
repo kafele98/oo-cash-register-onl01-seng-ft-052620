c5786233344da9a1fe2810a910e4578f9b8de3fc
Zoe Keller <keller.zoe98@gmail.com>
2020-06-11 13:40:57 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@ class CashRegister
   attr_reader :discount 
   attr_accessor :total 
   
-  def initialize 0
+  def initialize(discount = 0)
     @total = 0 
     @discount = discount 
     @items = []
