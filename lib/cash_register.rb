186bad23e4059b68ad3c865a7787fd727569e498
Zoe Keller <keller.zoe98@gmail.com>
2020-06-11 13:44:21 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,13 +8,21 @@ class CashRegister
     @items = []
   end 
   
-  def add_item (title, price, quantity= 1)
-    quantity.times do 
-      @items.push title  
-    end 
-    @total += price * quantity 
-    @items 
-  end 
+
+  def add_item(item, price, quantity = 1)
+    @price = price
+    @total += price * quantity
+    if quantity > 1
+      counter = 0
+      while counter < quantity
+        @items << item
+        counter += 1
+      end
+    else
+      @items << item
+    end
+  end
+
   
   def apply_discount 
     @total -= (@total * @discount.to_f / 100).round 
