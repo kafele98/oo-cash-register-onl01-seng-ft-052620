7d54f5f9d07b6609efade56d3b2d25edc83b9d51
Zoe Keller <keller.zoe98@gmail.com>
2020-06-11 13:45:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,17 +10,8 @@ class CashRegister
   
 
   def add_item(item, price, quantity = 1)
-    @price = price
-    @total += price * quantity
-    if quantity > 1
-      counter = 0
-      while counter < quantity
-        @items << item
-        counter += 1
-      end
-    else
-      @items << item
-    end
+    @ total += price * quantity 
+    
   end
 
   
